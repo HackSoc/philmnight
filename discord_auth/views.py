46f37baa2b40b9ffecfd9dd46522811213308353
Daniel O'Brien <dob9601@gmail.com>
2019-11-17 16:19:47 +0000

    Linting and tidying of discord_auth views.

--- discord_auth/views.py
+++ discord_auth/views.py
@@ -1,24 +1,30 @@
 """Views for handling discord authorisation."""
-import requests
 import random
 import string
+
+import requests
 from requests_oauthlib import OAuth2Session
 
-from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.db import IntegrityError
 
-DISCORD_REDIRECT_URI = 'https://discordapp.com/api/oauth2/authorize?client_id=644830838462218241&redirect_uri=https%3A%2F%2Fhacksoc-film-night.herokuapp.com%2Fdiscord%2Fverify&response_type=code&scope=identify%20guilds'
+DISCORD_REDIRECT_URI = ('https://discordapp.com/api/oauth2/authorize?client_id'
+                        '=644830838462218241&redirect_uri=https%3A%2F%2Fhackso'
+                        'c-film-night.herokuapp.com%2Fdiscord%2Fverify&respons'
+                        'e_type=code&scope=identify%20guilds')
 
 API_ENDPOINT = 'https://discordapp.com/api/v6'
 CLIENT_ID = '644830838462218241'
 CLIENT_SECRET = 'rIdG5AnCCWu-eap5-Myasd7BFnXHr6L9'
 REDIRECT_URI = 'https://hacksoc-film-night.herokuapp.com/discord/verify'
 
+COMPSCI_YORK_ID = '615499539218169866'
+
 
 def make_session(token=None, state=None, scope=None):
+    """Create an OAuth2Session with discord's api."""
     return OAuth2Session(
         client_id=CLIENT_ID,
         token=token,
@@ -28,54 +34,58 @@ def make_session(token=None, state=None, scope=None):
 
 
 def auth(request):
+    """Redirect a user to discord's auth page."""
+    # Skip auth process if user already logged in
+    if request.user:
+        return HttpResponseRedirect('/')
     return HttpResponseRedirect(DISCORD_REDIRECT_URI)
 
 
 def verify(request):
-    code = request.GET.get('code', None)
+    """Verify a provided code against discord's servers."""
+    # Skip verification process if user already logged in
+    if not request.user:
+        code = request.GET.get('code', None)
 
+        data = {
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': REDIRECT_URI,
+            'scope': 'identify email connections'
+        }
 
-    data = {
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': REDIRECT_URI,
-        'scope': 'identify email connections'
-    }
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    r = requests.post('%s/oauth2/token' % API_ENDPOINT, data=data, headers=headers)
-    try:
-        r.raise_for_status()
-        user_authorised = True
-    except requests.exceptions.HTTPError:
-        user_authorised = False
+        # Required headers for discord api request
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        token_request = requests.post('%s/oauth2/token' % API_ENDPOINT,
+                                      data=data, headers=headers)
 
+        discord = make_session(token=token_request.json())
 
-    if user_authorised:
-        discord = make_session(token=r.json())
+        user_data = discord.get(API_ENDPOINT+'/users/@me').json()
+        user_guilds = discord.get(API_ENDPOINT+'/users/@me/guilds').json()
 
-        user_data = discord.get(API_ENDPOINT + '/users/@me').json()
-        user_guilds = discord.get(API_ENDPOINT + '/users/@me/guilds').json()
+        compsci_discord = False
+        for guild in user_guilds:
+            if guild['id'] == COMPSCI_YORK_ID:
+                compsci_discord = True
 
-    compsci_discord = False
-    for guild in user_guilds:
-        if guild['id'] == '615499539218169866':
-            compsci_discord = True
-
-    
-        if compsci_discord:
-            try:
-                user = User.objects.create_user(user_data['username']+'#'+user_data['discriminator'], password=''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)]))
-                user.email = user_data['id']
-                user.save()
-            except IntegrityError:
-                user = User.objects.get(email = user_data['id'])
-            login(request, user)
-
+            if compsci_discord:
+                try:
+                    username = (user_data['username'] +
+                                '#' + user_data['discriminator'])
+                    password = ''.join([random.choice(
+                        string.ascii_letters+string.digits) for n in range(32)]
+                                      )
+                    user = User.objects.create_user(username,
+                                                    password=password)
+                    user.email = user_data['id']
+                    user.save()
+                except IntegrityError:
+                    user = User.objects.get(email=user_data['id'])
+                login(request, user)
 
     return HttpResponseRedirect('/')
-
-
---
+++ film_management/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Film(models.Model):
+    name = models.CharField(max_length=70, blank=False)
+    vote_count = models.IntegerField(default=0)
+    in_current_vote = models.BooleanField(default=False)
