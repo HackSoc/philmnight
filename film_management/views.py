c870d24e6a2a3c0abd2ba718119ead0a7f08c041
Daniel O'Brien <dob9601@gmail.com>
2019-11-18 12:35:36 +0000

    Addition of function to test whether current week is filmweek

--- film_management/views.py
+++ film_management/views.py
@@ -1,7 +1,13 @@
+from datetime import date
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
+def is_filmweek():
+    return date.now().isocalendar()[1] % 2 == 0
+
+
 @login_required
 def dashboard(request):
-    return render(request, 'film_management/dashboard.html', {})
+    return render(request, 'film_management/dashboard.html', {'is_filmweek': is_filmweek()})
