4482f832e39fcd75600f2f23613d3bb8b9dd900a
Daniel <dob9601@gmail.com>
2020-04-24 12:26:24 +0100

    Scheduling updated so that voting starts on friday but ends at 7 on the same day

--- film_management/views.py
+++ film_management/views.py
@@ -20,13 +20,12 @@ FILM_TIMEOUT = 10
 def get_phase():
     """Return the current phase of voting."""
     iso_date = datetime.date.today().isocalendar()
-    if iso_date[1] % 2 == 0 and iso_date[2] <= 5:
-        if iso_date[2] == 5:
+    if iso_date[1] % 2 == 0 and iso_date[2] == 5:
+        if datetime.datetime.now().hour >= 7:
             return 'filmnight'
         else:
             return 'voting'
-    else:
-        return 'submissions'
+    return 'submissions'
 
 
 def get_config():
